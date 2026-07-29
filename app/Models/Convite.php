6374aaeb5a5965e531982fcc1a04a2df8f70cf82
carlos-andemberg <cinfoandemberg@gmail.com>
2026-07-29 01:57:54 -0300

    SMTP-Update

--- app/Models/Convite.php
+++ app/Models/Convite.php
@@ -65,8 +65,7 @@ class Convite extends Model
 
         Mail::send('emails.acesso', $data, function($message) use ($to_name, $to_email) {
             $message->to($to_email, $to_name)
-                ->subject('Acesso a plataforma documentos_ifal');
-                $message->from('normativas@nees.com.br','Normativas - NEES');
+                ->subject('Acesso a plataforma CentralDoc');
         });
     }
 
